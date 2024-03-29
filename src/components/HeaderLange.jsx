0f9f56afa4398f8445614b844826e1d794b33ee6
Lars Knoke <larz.knoke@gmail.com>
2024-03-29 15:01:35 +0100

    kontakt header

--- src/components/HeaderLange.jsx
+++ src/components/HeaderLange.jsx
@@ -46,34 +46,57 @@ const HeaderLange = () => {
             </Link>
           </div>
           {/* End Logo */}
+          <div class="d-flex flex-column align-items-end gap-2">
+            <div className="header-kontakt d-flex gap-4">
+              <div className="d-flex gap-2">
+                <img
+                  width={15}
+                  src="/images/lange/icons/phone.svg"
+                  alt="Phone Icon"
+                />
+                <span>05284 - 94420</span>
+              </div>
+              <div className="d-flex gap-2">
+                <img
+                  width={15}
+                  src="/images/lange/icons/envelope.svg"
+                  alt="Email Icon"
+                />
+                <span>
+                  <a href="mailto:info@lange-malermeister.de">
+                    info@lange-malermeister.de
+                  </a>
+                </span>
+              </div>
+            </div>
+            <nav id="mega-menu-holder" className="navbar navbar-expand-lg">
+              <div className="container nav-container">
+                <div
+                  className="navbar-collapse collapse"
+                  id="navbarSupportedContent"
+                >
+                  <div className="d-lg-flex align-items-center">
+                    <MegaMenu />
+                    {/* End MegaMenu */}
 
-          <nav id="mega-menu-holder" className="navbar navbar-expand-lg">
-            <div className="container nav-container">
-              <div
-                className="navbar-collapse collapse"
-                id="navbarSupportedContent"
-              >
-                <div className="d-lg-flex align-items-center">
-                  <MegaMenu />
-                  {/* End MegaMenu */}
-
-                  <ul className="right-button-group d-flex align-items-center justify-content-center">
-                    {/* <li>
+                    <ul className="right-button-group d-flex align-items-center justify-content-center">
+                      {/* <li>
                       <Link href="/login" className="signIn-action">
                         Login
                       </Link>
                     </li> */}
-                    <li>
-                      <Link href="#kontakt" className="theme-btn-five">
-                        Kontakt
-                      </Link>
-                    </li>
-                  </ul>
-                  {/* End right-button-group  */}
+                      <li>
+                        <Link href="#kontakt" className="theme-btn-five">
+                          Kontakt
+                        </Link>
+                      </li>
+                    </ul>
+                    {/* End right-button-group  */}
+                  </div>
                 </div>
               </div>
-            </div>
-          </nav>
+            </nav>
+          </div>
           {/* End nav */}
         </div>
 
